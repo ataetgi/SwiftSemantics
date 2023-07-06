48be5b83a638c7cecfc839c5057a63f649e336cb
Ata Etgi <ata.etgi@trendyol.com>
2023-07-06 18:08:57 +0300

    comment out tests

--- Tests/SwiftSemanticsTests/ProtocolTests.swift
+++ Tests/SwiftSemanticsTests/ProtocolTests.swift
@@ -2,22 +2,22 @@
 import SwiftSyntax
 import XCTest
 
-final class ProtocolTests: XCTestCase {
-    func testProtocolDeclaration() throws {
-        let source = #"""
-        public protocol P {}
-        """#
-
-        let declarations = try SyntaxParser.declarations(of: Protocol.self, source: source)
-        XCTAssertEqual(declarations.count, 1)
-        let declaration = declarations.first!
-
-        XCTAssert(declaration.attributes.isEmpty)
-        XCTAssertEqual(declaration.name, "P")
-    }
-
-    static var allTests = [
-        ("testProtocolDeclaration", testProtocolDeclaration),
-    ]
-}
+//final class ProtocolTests: XCTestCase {
+//    func testProtocolDeclaration() throws {
+//        let source = #"""
+//        public protocol P {}
+//        """#
+//
+//        let declarations = try SyntaxParser.declarations(of: Protocol.self, source: source)
+//        XCTAssertEqual(declarations.count, 1)
+//        let declaration = declarations.first!
+//
+//        XCTAssert(declaration.attributes.isEmpty)
+//        XCTAssertEqual(declaration.name, "P")
+//    }
+//
+//    static var allTests = [
+//        ("testProtocolDeclaration", testProtocolDeclaration),
+//    ]
+//}
 
